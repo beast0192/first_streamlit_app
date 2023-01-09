8a3dbe2fdd33e2be21752d0237844b11b71f5fbd
beast0192 <119415266+beast0192@users.noreply.github.com>
2023-01-09 16:45:02 +0530

    Update create_streamlit_app.py

--- create_streamlit_app.py
+++ create_streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas as pd
+import pandas 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 & Blueberry otamale ')
