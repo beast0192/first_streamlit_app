c9af8904e174b8cd7c37265cbb147855122cd3c4
beast0192 <119415266+beast0192@users.noreply.github.com>
2023-01-09 16:39:43 +0530

    Update create_streamlit_app.py

--- create_streamlit_app.py
+++ create_streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry otamale ')
+streamlit.text('kale,spinich & rocket Smoothiee')
+streamlit.text('hard bolied freerange egg')
